d61191c300a1f325356eaf78061c98572ab36f11
Berkay Özdağ <berkayozdag2001@gmail.com>
2021-02-19 11:38:23 +0300

    ComponentYapısıGiriş

---
+++ vue-cli-intro/src/LampStatus.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Lamba durumu : {{status}} </p>
+    <button @click="openLambs">Lambayı aç</button>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  data : function (){
+    return {
+      status : "kapalı"
+    }
+  },
+  methods : {
+    openLambs(){
+      this.status = "açık";
+    }
+  }
+}
+</script>
